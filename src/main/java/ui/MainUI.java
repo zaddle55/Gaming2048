3434901404cd685fe437440b94b06519d747b00a
zaddle55 <12310823@mail.sustech.edu.cn>
2024-04-22 22:32:25 +0800

    test javafx

---
+++ src/main/java/ui/MainUI.java
@@ -0,0 +1,20 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class MainUI {
+    @FXML
+    private void initialize() {
+        // TODO
+    }
+
+    @FXML
+    private void handleButtonAction() {
+        // TODO
+    }
+
+    @FXML
+    public void MouseEnterLabelAction(MouseEvent mouseEvent) {
+    }
+}
---
+++ src/test/java/TestJavaFXBootstrap.java
@@ -0,0 +1,5 @@
+public class TestJavaFXBootstrap {
+    public static void main(String[] args) {
+        TestMainUIApplication.main(args);
+    }
+}
