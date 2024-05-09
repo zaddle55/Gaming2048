227ef0655bad845bbd77973ffc325ac088470373
zaddle55 <chenyifan20050115@126.com>
2024-05-09 14:09:53 +0800

    add timer

--- src/main/java/util/Timer.java
+++ src/main/java/util/Timer.java
@@ -1,4 +1,37 @@
+
 package util;
 
-public class Timer {
+import javafx.animation.AnimationTimer;
+
+public abstract class Timer {
+    private long startTime;
+    private long endTime;
+    private boolean isRunning;
+
+    // 模式选择
+    public static int COUNT_UP = 0;
+    public static int COUNT_DOWN = 1;
+
+    // 计时器模式
+    public Timer(int mode, Time var) {
+        if (mode == COUNT_UP) {
+            this.startTime = System.currentTimeMillis();
+        } else {
+            this.startTime = 0;
+        }
+    }
+
+    public abstract void start();
+
+    public void stop() {
+        this.endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime() {
+        return this.endTime - this.startTime;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
