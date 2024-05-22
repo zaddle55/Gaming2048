002b9c9ccbb9feb856525238fb04430ee0aa8509
SUSTechCJK2023 <2444977612@qq.com>
2024-05-22 17:10:50 +0800

    package model; import java.util.List;
    public class UserManager {
        List<User> userList;
    
        public UserManager() {
    
        }
    
        public void register(String name, String password) {
    
        }
        public void logIn(String name, String password) {
    
        }
        public boolean hasUser(String name) {
    
        }
        public boolean isPasswordValid(String password) {
    
        }
    }

--- src/main/java/model/UserManager.java
+++ src/main/java/model/UserManager.java
@@ -1,4 +1,23 @@
 package model;
+import java.util.List;
 
 public class UserManager {
+    List<User> userList;
+
+    public UserManager() {
+
+    }
+
+    public void register(String name, String password) {
+
+    }
+    public void logIn(String name, String password) {
+
+    }
+    public boolean hasUser(String name) {
+
+    }
+    public boolean isPasswordValid(String password) {
+
+    }
 }
