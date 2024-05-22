c94375844e63ff9d93d79ebbd9fe8692973fa70f
SUSTechCJK2023 <2444977612@qq.com>
2024-05-22 17:09:43 +0800

    public User (String name, String password, String path) {         this.name = name;         this.password = password;         this.path = path;     }

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -4,4 +4,9 @@ public class User {
     private String name;
     private String password;
     private String path;
+    public User (String name, String password, String path) {
+        this.name = name;
+        this.password = password;
+        this.path = path;
+    }
 }
