f95bc01c311c14d84cd154bec96fcec3fe65e7d6
SUSTechCJK2023 <2444977612@qq.com>
2024-05-28 12:09:33 +0800

    package ai;
    import controller.GameUI;
    import util.Direction;
    
    /**
     * AlphaDuo
     * @version: 1.0.0
     * @description: AlphaDuo is a class that represents the AI of the game, "duo" means two, which means that the AI is designed for 2048 which uses "2" grids to make larger binary numbers.
     * @function: Used to play the game automatically or to provide hints to the player.
     * @Implementation: AlphaDuo uses the Minimax and Alpha-Beta algorithms to simulate the game and make decisions.
     */
    public class AlphaDuo {
        protected static double evaluationScore = 0;
        protected static final double monotonyWeight = 0.4; // 单调性权重
        protected static final double smoothWeight = 1.0; // 平滑性权重
        protected static final double emptyWeight = 0.7; // 总空格数权重
        protected static final double islandWeight = 0.1; // 孤立空格数权重
        protected static int directionNum = 0;
    
        protected static double evaluate(Direction d) {
            int monotonyRowL = 0;
            int monotonyRowR = 0;
            int monotonyColumnU = 0;
            int monotonyColumnD = 0;
            for (int j=0; j < GameUI.getGrid().getSize(); j++) {
                for (int i=0; i < GameUI.getGrid().getSize(); i++) {
                    for (int a=0; j+a < GameUI.getGrid().getSize()-1; a++) {
                        if ((GameUI.getGrid().getBoard()[i][j+a] < GameUI.getGrid().getBoard()[i][j+a+1]) || (GameUI.getGrid().getBoard()[i][j+a] > GameUI.getGrid().getBoard()[i][j+a+1])) {
                            monotonyRowL += 1;
                        }
                    }
                    for (int b=0; i+b < GameUI.getGrid().getSize() - 1; b++) {
                        if (GameUI.getGrid().getBoard()[i+b][j] < GameUI.getGrid().getBoard()[i+b+1][j]) {
                            monotonyColumnD += 1;
                        } else if (GameUI.getGrid().getBoard()[i+b][j] > GameUI.getGrid().getBoard()[i+b+1][j]) {
                            monotonyColumnU += 1;
                        }
                    }
                }
            }
            evaluationScore += (monotonyRowL * monotonyWeight) + (monotonyRowR * monotonyWeight) + (monotonyColumnU * monotonyWeight) + (monotonyColumnD * monotonyWeight);
    
            int emptyTile = 0;
            for (int i=0; i < GameUI.getGrid().getSize(); i++) {
                for (int j=0; j < GameUI.getGrid().getSize(); j++) {
                    if (GameUI.getGrid().getBoard()[i][j] == 0) {
                        emptyTile += 1;
                    }
                }
            }
            evaluationScore += emptyTile * emptyWeight;
    
            return evaluationScore;
        }
        protected static int setDirection() {
            Direction direction = Direction.UP;
            if (evaluate(Direction.DOWN) > evaluate(direction)) {
                direction = Direction.DOWN;
                directionNum = 1;
            }
            if (evaluate(Direction.LEFT) > evaluate(direction)) {
                direction = Direction.LEFT;
                directionNum = 2;
            }
            if (evaluate(Direction.RIGHT) > evaluate(direction)) {
                directionNum = 3;
            }
            return directionNum;
        }
    }

--- src/main/java/ai/AlphaDuo.java
+++ src/main/java/ai/AlphaDuo.java
@@ -1,6 +1,8 @@
 package ai;
 
-import model.Grid;
+import controller.GameUI;
+import util.Direction;
+
 /**
  * AlphaDuo
  * @version: 1.0.0
@@ -9,18 +11,65 @@ import model.Grid;
  * @Implementation: AlphaDuo uses the Minimax and Alpha-Beta algorithms to simulate the game and make decisions.
  */
 public class AlphaDuo {
-    public static int[] DFS(Grid grid, int curDepth, int finDepth, int alpha, int beta) {
-        // do something...
-        return null;
-    }
+    protected static double evaluationScore = 0;
+    protected static final double monotonyWeight = 0.4; // 单调性权重
+    protected static final double smoothWeight = 1.0; // 平滑性权重
+    protected static final double emptyWeight = 0.7; // 总空格数权重
+    protected static final double islandWeight = 0.1; // 孤立空格数权重
+    protected static int directionNum = 0;
 
-    public static int generateMove(Grid grid) {
-        // do something...
-        return 0;
-    }
+    protected static double evaluate(Direction d) {
+        int monotonyRowL = 0;
+        int monotonyRowR = 0;
+        int monotonyColumnU = 0;
+        int monotonyColumnD = 0;
+        for (int j=0; j < GameUI.getGrid().getSize(); j++) {
+            for (int i=0; i < GameUI.getGrid().getSize(); i++) {
+                for (int a=0; j+a < GameUI.getGrid().getSize()-1; a++) {
+                    if ((GameUI.getGrid().getBoard()[i][j+a] < GameUI.getGrid().getBoard()[i][j+a+1]) || (GameUI.getGrid().getBoard()[i][j+a] > GameUI.getGrid().getBoard()[i][j+a+1])) {
+                        monotonyRowL += 1;
+                    }
+                }
+                for (int b=0; i+b < GameUI.getGrid().getSize() - 1; b++) {
+                    if (GameUI.getGrid().getBoard()[i+b][j] < GameUI.getGrid().getBoard()[i+b+1][j]) {
+                        monotonyColumnD += 1;
+                    } else if (GameUI.getGrid().getBoard()[i+b][j] > GameUI.getGrid().getBoard()[i+b+1][j]) {
+                        monotonyColumnU += 1;
+                    }
+                }
+            }
+        }
+        evaluationScore += (monotonyRowL * monotonyWeight) + (monotonyRowR * monotonyWeight) + (monotonyColumnU * monotonyWeight) + (monotonyColumnD * monotonyWeight);
 
-    public static int evaluate(Grid grid) {
-        // do something...
-        return 0;
+
+        int emptyTile = 0;
+        for (int i=0; i < GameUI.getGrid().getSize(); i++) {
+            for (int j=0; j < GameUI.getGrid().getSize(); j++) {
+                if (GameUI.getGrid().getBoard()[i][j] == 0) {
+                    emptyTile += 1;
+                }
+            }
+        }
+        evaluationScore += emptyTile * emptyWeight;
+
+
+
+
+        return evaluationScore;
+    }
+    protected static int setDirection() {
+        Direction direction = Direction.UP;
+        if (evaluate(Direction.DOWN) > evaluate(direction)) {
+            direction = Direction.DOWN;
+            directionNum = 1;
+        }
+        if (evaluate(Direction.LEFT) > evaluate(direction)) {
+            direction = Direction.LEFT;
+            directionNum = 2;
+        }
+        if (evaluate(Direction.RIGHT) > evaluate(direction)) {
+            directionNum = 3;
+        }
+        return directionNum;
     }
 }
